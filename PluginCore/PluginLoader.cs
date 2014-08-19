f2e2ee0e767096ca07e16971b9f4a26d95d65e27
andy <andy@andydote.co.uk>
2014-08-19 20:14:53 +0100

    pluginCore: initialise bus on each plugin

--- PluginCore/PluginLoader.cs
+++ PluginCore/PluginLoader.cs
@@ -10,10 +10,13 @@ namespace PluginCore
 		private readonly IBus _bus;
 		private readonly HashSet<string> _pluginPaths;
 
+		private readonly List<Plugin> _runningPlugins;
+
 		public PluginLoader(IBus bus)
 		{
 			_bus = bus;
 			_pluginPaths = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+			_runningPlugins = new List<Plugin>();
 		}
 
 		public void Add(string pluginPath)
@@ -31,7 +34,13 @@ namespace PluginCore
 			var definitions = new PluginPreLoader().BuildDefinitions(_pluginPaths);
 			var sorted = new PluginGraph(_bus).Build(definitions);
 			
-			sorted.ToList().ForEach(d => d.Run());
+			sorted.ToList().ForEach(d =>
+			{
+				var plugin = d.Run();
+				plugin.SetupBus(_bus);
+
+				_runningPlugins.Add(plugin);
+			});
 		}
 	}
 }
