4f74dc598151699af3bfcbc4e635b306b93fa114
andy <andy@andydote.co.uk>
2014-08-22 00:43:18 +0100

    pluginCore.tests: add logging bus for testing with

---
+++ PluginCore.Tests/TestUtils/LoggingBus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
+using PluginCore.Bus;
+
+namespace PluginCore.Tests.TestUtils
+{
+	public class LoggingBus : IBus
+	{
+		public Dictionary<Type, List<Action<object>>> Subscribers { get; private set; }
+		public List<object> MessagesPublished { get; private set; }
+
+		private readonly IBus _other;
+
+		public LoggingBus() : this(Substitute.For<IBus>())
+		{
+		}
+
+		public LoggingBus(IBus other)
+		{
+			_other = other;
+			Subscribers = new Dictionary<Type, List<Action<object>>>();
+			MessagesPublished = new List<object>();
+		}
+		
+		public void Subscribe<TMessage>(Action<TMessage> handler)
+		{
+			var messageType = typeof(TMessage);
+
+			if (Subscribers.ContainsKey(messageType) == false)
+			{
+				Subscribers[messageType] = new List<Action<object>>();
+			}
+
+			Subscribers[messageType].Add(message => handler((TMessage)message));
+			_other.Subscribe<TMessage>(handler);
+		}
+
+		public void Publish<TMessage>(TMessage message)
+		{
+			MessagesPublished.Add(message);
+			_other.Publish<TMessage>(message);
+		}
+	}
+}
