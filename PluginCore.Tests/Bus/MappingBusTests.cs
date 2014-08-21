e2ff57d7b9ea2d5d6c0831e39b3fabbcc45ce9ba
andy <andy@andydote.co.uk>
2014-08-21 12:51:25 +0100

    tests: add pluginCore test project

---
+++ PluginCore.Tests/Bus/MappingBusTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace PluginCore.Tests.Bus
+{
+	public class MappingBusTests
+	{
+		[Fact]
+		public void A_message_is_mapped_to_all_messages_of_the_same_name()
+		{
+			var bus = new mappingbus
+		}
+	}
+
+
+	namespace First
+	{
+		public class TestMessage
+		{
+			public string Message { get; set; }
+		}
+	}
+
+	namespace Second
+	{
+		public class TestMessage
+		{
+			public string Message { get; set; }
+		}
+	}
+}
