8255ab286cec4215410a48b43973e61fc36344f1
andy <andy@andydote.co.uk>
2014-08-22 00:22:52 +0100

    pluginCore.tests: add Catch class for lambda exception catching

---
+++ PluginCore.Tests/TestUtils/Catch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace PluginCore.Tests.TestUtils
+{
+	public class Catch
+	{
+		public static Exception Exception(Action action)
+		{
+			try
+			{
+				action();
+			}
+			catch (Exception ex)
+			{
+				return ex;
+			}
+
+			return null;
+		}
+
+		public static T Exception<T>(Action action) where T : Exception
+		{
+			try
+			{
+				action();
+			}
+			catch (T ex)
+			{
+				return ex;
+			}
+
+			return null;
+		}
+	}
+}
